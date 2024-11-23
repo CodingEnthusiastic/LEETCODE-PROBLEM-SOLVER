4c7abff9a284c19bb91eaee5510d92cbf48debb2
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2024-11-23 13:08:41 +0530

    Time: 454 ms (5.83%) | Memory: 61.9 MB (9.51%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        vector<vector<char>>result(box[0].size(),vector<char>(box.size(),'\0'));
+        vector<vector<int>>stores;
+        for(int i=0;i<box.size();i++)
+        {
+            int countbox=0;
+            vector<int>temp;
+            for(int j=0;j<box[i].size();j++)
+            {
+                if(box[i][j]=='*')
+                {
+                    temp.push_back(countbox);
+                    countbox=0;
+                }
+                else if(box[i][j]=='#')
+                {
+                    box[i][j]='.';
+                    countbox++;
+                }
+                else if(box[i][j]=='.')
+                {
+                    
+                    continue;
+                }
+            }
+            temp.push_back(countbox);
+            stores.push_back(temp);
+        }
+
+        for(int i=box.size()-1;i>=0;i--)
+        {
+            int k=stores[i].size()-1;
+            for(int j=box[0].size()-1;j>=0;j--)
+            {
+                if(k<0)
+                    break;
+                int stone=stores[i][k];
+                cout<<stone<<' ';
+                while((j>=0)&&(stone>0))
+                {
+                    box[i][j]='#';
+                    stone--;
+                    j--;
+                }
+                k--;
+                while((j>=0)&&(box[i][j]!='*'))
+                {
+                    j--;
+                }
+            }
+        }
+
+        // for(int i=0;i<box.size();i++)
+        // {
+        //     for(int j=0;j<box[i].size()/2;j++)
+        //     {
+        //         char c=box[i][j];
+        //         box[i][j]=box[i][box[i].size()-j-1];
+        //         box[i][box[i].size()-j-1]=c;
+        //     }
+        // }
+        for(int i=0;i<box.size();i++)
+        {
+            for(int j=0;j<box[i].size();j++)
+            {
+                cout<<box[i][j]<<' ';
+                result[j][i]=box[i][j];
+            }
+            cout<<endl;
+        }
+        for(int i=0;i<result.size();i++)
+        {
+            for(int j=0;j<((result[i].size())/2);j++)
+            {
+                char c=result[i][j];
+                result[i][j]=result[i][result[i].size()-j-1];
+                result[i][result[i].size()-j-1]=c;
+            }
+        }
+        
+        return result;
+    }
+};
