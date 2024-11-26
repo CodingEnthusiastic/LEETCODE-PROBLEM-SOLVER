495873c23ddcc8d63539521f90456f75bc6eafc1
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2024-11-26 11:48:23 +0530

    Time: 6 ms (70.56%) | Memory: 93.6 MB (67.77%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int>inward(n,0);
+        vector<int>outward(n,0);
+        for(int i=0;i<edges.size();i++)
+        {
+            outward[edges[i][0]]++;
+            inward[edges[i][1]]++;
+        }
+        int winner=n;
+        for(int i=0;i<n;i++)
+        {
+            if(inward[i]==0)
+            {
+                if(winner==n)
+                {
+                    winner=i;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+
+        if(winner==n)
+        {
+            return -1;
+        }
+        return winner;
+    }
+};
