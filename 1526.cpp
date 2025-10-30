054a5d1a98e82dee8be1733bf30c16d22420d84f
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2025-10-30 19:13:02 +0530

    Create 1526.cpp

---
+++ 1526.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int ans=target[0];
+        for(int i=0;i<target.size()-1;i++)
+        {
+            if(target[i+1]-target[i] > 0)
+            ans+=(target[i+1]-target[i]);
+        }
+
+        return ans;
+    }
+};
