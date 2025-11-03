f29a8676420608a0a5ae925dc3edc64d4e86f28f
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2025-11-03 17:29:38 +0530

    Create minimum-time-to-make-rope-colorful.cpp

---
+++ minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int ans = 0;
+        int n = colors.size();
+        int sum = neededTime[0];
+        int maxi = neededTime[0];
+
+        for (int i = 1; i < n; i++) 
+        {
+            if (colors[i] == colors[i - 1]) 
+            {
+                sum += neededTime[i];
+                maxi = max(maxi, neededTime[i]);
+            } 
+            else 
+            {
+                ans += sum - maxi; 
+                sum = neededTime[i];
+                maxi = neededTime[i];
+            }
+        }
+        ans += sum - maxi; 
+        return ans;
+    }
+};
