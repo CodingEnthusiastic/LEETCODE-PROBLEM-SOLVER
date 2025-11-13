2a4481bf600d50aff80961406597c1bde00ac0cd
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2025-11-13 19:15:00 +0530

    Implement maxOperations to count moves for ones

---
+++ maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int cnt=0;
+        int n=s.size();
+        int cnt1=(s[0]=='1');
+        for(int i=1; i<n; i++)
+        {
+            cnt1+=s[i]=='1';
+            cnt+=(-(s[i]=='0' &&s[i-1]=='1'))&cnt1;
+        }
+        return cnt;
+    }
+};
