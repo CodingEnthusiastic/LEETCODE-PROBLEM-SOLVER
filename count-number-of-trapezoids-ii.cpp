beb21bb7f310903c0b06dbdd8a1c24a954803185
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2025-12-03 19:17:16 +0530

    Implement countTrapezoids function in Solution class

---
+++ count-number-of-trapezoids-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        int n = points.size();
+        int inf = 1e9 + 7;
+        unordered_map<float, vector<float>> slope;
+        unordered_map<int, vector<float>> midi;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int dx = x1 - x2;
+                int dy = y1 - y2;
+                float k, b;
+                if (x2 == x1) {
+                    k = inf;
+                    b = x1;
+                } else {
+                    k = (float)(y2 - y1) / (x2 - x1);
+                    b = (float)(y1 * dx - x1 * dy) / dx;
+                }
+                int mid = (x1 + x2) * 10000 + (y1 + y2);
+                slope[k].push_back(b);
+                midi[mid].push_back(k);
+            }
+        }
+        for (auto& [_, sti] : slope) {
+            if (sti.size() == 1) {
+                continue;
+            }
+            map<float, int> cnt;
+            for (float b : sti) {
+                cnt[b]++;
+            }
+            int sum = 0;
+            for (auto& [_, count] : cnt) {
+                ans += sum * count;
+                sum += count;
+            }
+        }
+        for (auto& [_, mts] : midi) {
+            if (mts.size() == 1) {
+                continue;
+            }
+            map<float, int> cnt;
+            for (float k : mts) {
+                cnt[k]++;
+            }
+            int sum = 0;
+            for (auto& [_, count] : cnt) {
+                ans -= sum * count;
+                sum += count;
+            }
+        }
+        return ans;
+    }
+};
