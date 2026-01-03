c620951138b66dfb7ffb4580dcd4841e5161dfeb
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2026-01-03 11:01:50 +0530

    Add solution for painting n x 3 grid problem
    Implement a solution to calculate the number of ways to paint a n x 3 grid with constraints.

---
+++ number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numOfWays(int n) 
+    {
+        const int MOD = 1000000007;
+        long long a = 6, b = 6;
+
+        for (int i = 2; i <= n; i++) {
+            long long newa = (2 * a + 2 * b) % MOD;
+            long long newb = (2 * a + 3 * b) % MOD;
+            a = newa;
+            b = newb;
+        }
+
+        return (a + b) % MOD;
+    }
+};
