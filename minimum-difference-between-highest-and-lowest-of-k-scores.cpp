03c4595bf7c0e5597f0445748154a639dd2320dc
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2026-01-25 18:16:53 +0530

    Add minimumDifference function in Solution class
    Implement a solution to find the minimum difference between the highest and lowest of k scores.

---
+++ minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int mini=nums[k-1]-nums[0];
+        for(int i=0;i<n-k+1;i++)
+        {
+            mini=min(mini,nums[k+i-1]-nums[i]);
+        }
+        return mini;
+    }
+};
